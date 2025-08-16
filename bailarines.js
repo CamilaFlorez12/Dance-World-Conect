de6bbd48e33ca9afe431386f67e4b871900bd02b
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-15 23:15:37 -0500

    feat: :sparkles: insert bailarines
    insert info to bailarines
    
    By@CamilaFlorez

---
+++ bailarines.js
@@ -0,0 +1,123 @@
+db.bailarines.insertMany([
+    {
+        nombre: 'Luna Camila',
+        apellido: 'Rojas Sanchez',
+        email: 'camilar@gmail.com',
+        fecha_nacimiento: '2007-07-08',
+        'país': 'Colombia',
+        estilos: [
+            'ballet',
+            'salsa',
+            'bambuco',
+            'joropo'
+        ],
+        anios_experiencia: 12
+    }, {
+        nombre: 'Mateo Andrés',
+        apellido: 'Gómez Pérez',
+        email: 'mateog@gmail.com',
+        fecha_nacimiento: '2005-03-15',
+        'país': 'Colombia',
+        estilos: [
+            'hip hop',
+            'breakdance',
+            'locking'
+        ],
+        anios_experiencia: 8
+    }, {
+        nombre: 'Valentina',
+        apellido: 'Martínez Torres',
+        email: 'valentina.m@gmail.com',
+        fecha_nacimiento: '2006-11-22',
+        'país': 'Argentina',
+        estilos: [
+            'ballet',
+            'contemporáneo',
+            'jazz'
+        ],
+        anios_experiencia: 5
+    }, {
+        nombre: 'Santiago',
+        apellido: 'Ramírez López',
+        email: 'santiago.r@gmail.com',
+        fecha_nacimiento: '2004-06-30',
+        'país': 'México',
+        estilos: [
+            'salsa',
+            'bachata',
+            'merengue'
+        ],
+        anios_experiencia: 9
+    }, {
+        nombre: 'Isabella',
+        apellido: 'Hernández Cruz',
+        email: 'isabella.h@gmail.com',
+        fecha_nacimiento: '2007-01-10',
+        'país': 'Chile',
+        estilos: [
+            'contemporáneo',
+            'jazz',
+            'hip hop'
+        ],
+        anios_experiencia: 7
+    }, {
+        nombre: 'Daniel',
+        apellido: 'Vargas Morales',
+        email: 'daniel.v@gmail.com',
+        fecha_nacimiento: '2005-09-18',
+        'país': 'Perú',
+        estilos: [
+            'salsa',
+            'tango',
+            'folklore'
+        ],
+        anios_experiencia: 11
+    }, {
+        nombre: 'Camila Sofía',
+        apellido: 'Pérez Jiménez',
+        email: 'camila.s@gmail.com',
+        fecha_nacimiento: '2006-12-05',
+        'país': 'Colombia',
+        estilos: [
+            'ballet',
+            'contemporáneo',
+            'danza urbana'
+        ],
+        anios_experiencia: 9
+    }, {
+        nombre: 'Lucas',
+        apellido: 'Ortiz Ramírez',
+        email: 'lucas.o@gmail.com',
+        fecha_nacimiento: '2004-08-21',
+        'país': 'Venezuela',
+        estilos: [
+            'hip hop'
+        ],
+        anios_experiencia: 1
+    }, {
+        nombre: 'Mariana',
+        apellido: 'Gutiérrez Flores',
+        email: 'mariana.g@gmail.com',
+        fecha_nacimiento: '2007-04-14',
+        'país': 'Colombia',
+        estilos: [
+            'salsa',
+            'ballet',
+            'danza contemporánea'
+        ],
+        anios_experiencia: 4
+    }, {
+        nombre: 'Julián',
+        apellido: 'Sánchez Díaz',
+        email: 'julian.s@gmail.com',
+        fecha_nacimiento: '2005-10-03',
+        'país': 'Ecuador',
+        estilos: [
+            'tango',
+            'folklore',
+            'samba'
+        ],
+        anios_experiencia: 3
+    }
+
+])
