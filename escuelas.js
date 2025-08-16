819b223c2559f8ce221897afbf4d4ed470da2ee7
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-15 23:16:55 -0500

    feat: :sparkles: insert escuelas
    insert info abaout escuelas
    
    by@CamilaFlorez

---
+++ escuelas.js
@@ -0,0 +1,121 @@
+db.escuelas.insertMany([
+    {
+        nombre_escuela: 'Cultural Dance Academy',
+        ciudad: 'Duitama',
+        'país': 'Colombia',
+        'estilos_enseñados': [
+            'ballet',
+            'folklore'
+        ],
+        id_instructores: [
+            '1',
+            '2'
+        ]
+    }, {
+        nombre_escuela: 'Urban Moves Studio',
+        ciudad: 'Medellín',
+        'país': 'Colombia',
+        'estilos_enseñados': [
+            'hip hop',
+            'breakdance'
+        ],
+        id_instructores: [
+            '4',
+            '8'
+        ]
+    }, {
+        nombre_escuela: 'Jazz & Contemporary Hub',
+        ciudad: 'Buenos Aires',
+        'país': 'Argentina',
+        'estilos_enseñados': [
+            'danza contemporánea',
+            'jazz'
+        ],
+        id_instructores: [
+            '3',
+            '5'
+        ]
+    }, {
+        nombre_escuela: 'Salsa y Ritmo',
+        ciudad: 'Cali',
+        'país': 'Colombia',
+        'estilos_enseñados': [
+            'salsa',
+            'tango'
+        ],
+        id_instructores: [
+            '6',
+            '9'
+        ]
+    }, {
+        nombre_escuela: 'Urban Dance Factory',
+        ciudad: 'Caracas',
+        'país': 'Venezuela',
+        'estilos_enseñados': [
+            'danza urbana',
+            'hip hop'
+        ],
+        id_instructores: [
+            '7',
+            '4'
+        ]
+    }, {
+        nombre_escuela: 'Break & Pop Academy',
+        ciudad: 'Caracas',
+        'país': 'Venezuela',
+        'estilos_enseñados': [
+            'breakdance',
+            'hip hop'
+        ],
+        id_instructores: [
+            '8',
+            '4'
+        ]
+    }, {
+        nombre_escuela: 'Tango Express',
+        ciudad: 'Quito',
+        'país': 'Ecuador',
+        'estilos_enseñados': [
+            'tango'
+        ],
+        id_instructores: [
+            '9'
+        ]
+    }, {
+        nombre_escuela: 'Samba School',
+        ciudad: 'Río de Janeiro',
+        'país': 'Brasil',
+        'estilos_enseñados': [
+            'samba'
+        ],
+        id_instructores: [
+            '10'
+        ]
+    }, {
+        nombre_escuela: 'Contemporary Flow',
+        ciudad: 'Santiago',
+        'país': 'Chile',
+        'estilos_enseñados': [
+            'danza contemporánea',
+            'jazz'
+        ],
+        id_instructores: [
+            '3',
+            '5'
+        ]
+    }, {
+        nombre_escuela: 'Dance Fusion Academy',
+        ciudad: 'Bogotá',
+        'país': 'Colombia',
+        'estilos_enseñados': [
+            'ballet',
+            'danza urbana',
+            'hip hop'
+        ],
+        id_instructores: [
+            '1',
+            '7',
+            '4'
+        ]
+    }
+])
