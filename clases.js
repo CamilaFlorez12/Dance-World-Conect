a59840c6f69cbfa343ca1da585f20093a7d90fa9
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-15 23:16:20 -0500

    feat: :sparkles: insert clases
    insert info to class
    
    ByCamilaFlores

---
+++ clases.js
@@ -0,0 +1,73 @@
+db.clases.insertMany([
+    {
+        estilo: 'folklore',
+        nivel: 'Avanzado',
+        id_instructor: '1',
+        fecha: new Date("2025-08-15"),
+        duracion_minutos: 60,
+        precio: 50000
+    }, {
+        estilo: 'ballet',
+        nivel: 'Intermedio',
+        id_instructor: '2',
+        fecha: new Date("2025-08-16"),
+        duracion_minutos: 75,
+        precio: 60000
+    }, {
+        estilo: 'danza contemporánea',
+        nivel: 'Principiante',
+        id_instructor: '3',
+        fecha: new Date("2025-08-17"),
+        duracion_minutos: 50,
+        precio: 45000
+    }, {
+        estilo: 'hip hop',
+        nivel: 'Avanzado',
+        id_instructor: '4',
+        fecha: new Date("2025-08-18"),
+        duracion_minutos: 60,
+        precio: 55000
+    }, {
+        estilo: 'jazz',
+        nivel: 'Intermedio',
+        id_instructor: '5',
+        fecha: new Date("2025-08-19"),
+        duracion_minutos: 70,
+        precio: 50000
+    }, {
+        estilo: 'salsa',
+        nivel: 'Principiante',
+        id_instructor: '6',
+        fecha: new Date("2025-08-20"),
+        duracion_minutos: 60,
+        precio: 48000
+    }, {
+        estilo: 'danza urbana',
+        nivel: 'Avanzado',
+        id_instructor: '7',
+        fecha: new Date("2025-08-21"),
+        duracion_minutos: 65,
+        precio: 52000
+    }, {
+        estilo: 'breakdance',
+        nivel: 'Intermedio',
+        id_instructor: '8',
+        fecha: new Date("2025-08-22"),
+        duracion_minutos: 55,
+        precio: 50000
+    }, {
+        estilo: 'tango',
+        nivel: 'Principiante',
+        id_instructor: '9',
+        fecha: new Date("2025-08-23"),
+        duracion_minutos: 60,
+        precio: 47000
+    }, {
+        estilo: 'samba',
+        nivel: 'Avanzado',
+        id_instructor: '10',
+        fecha: new Date("2025-08-24"),
+        duracion_minutos: 75,
+        precio: 60000
+    }
+])
