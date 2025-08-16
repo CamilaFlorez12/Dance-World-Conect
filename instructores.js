0b8fbeec221bf28b55c525f2b2b732aa45111d8e
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-15 23:18:06 -0500

    feat: :sparkles: insert instructores
    insert info about isntructores
    
    By@CamilaFlorez

---
+++ instructores.js
@@ -0,0 +1,143 @@
+db.instructores.insertMany([
+    {
+        nombre: 'Leidy Carolina',
+        apellido: 'Bello Gonzalez',
+        email: 'carobello@outlook.com',
+        especialidad: 'folklore',
+        certificaciones: [
+            'licenciatura en danza',
+            'pedagogía y didáctica',
+            'danza folclorica',
+            'anatomía y prevención de lesiones'
+        ],
+        'país': 'Colombia',
+        anios_experiecia: 20,
+        id_instructor: '1'
+    }, {
+        nombre: 'Andrés Felipe',
+        apellido: 'Gómez Torres',
+        email: 'andresf@gmail.com',
+        especialidad: 'ballet',
+        certificaciones: [
+            'licenciatura en danza',
+            'pedagogía y didáctica',
+            'ballet clásico',
+            'anatomía y prevención de lesiones'
+        ],
+        'país': 'Colombia',
+        anios_experiecia: 18,
+        id_instructor: '2'
+    }, {
+        nombre: 'Sofía',
+        apellido: 'Perez Martinez',
+        email: 'sofia.p@hotmail.com',
+        especialidad: 'danza contemporánea',
+        certificaciones: [
+            'licenciatura en danza',
+            'técnicas contemporáneas',
+            'coreografía avanzada',
+            'danza terapia'
+        ],
+        'país': 'Argentina',
+        anios_experiecia: 15,
+        id_instructor: '3'
+    }, {
+        nombre: 'Santiago',
+        apellido: 'Ramírez Díaz',
+        email: 'santiago.r@gmail.com',
+        especialidad: 'hip hop',
+        certificaciones: [
+            'licenciatura en danza urbana',
+            'pedagogía y didáctica',
+            'street dance',
+            'coreografía avanzada'
+        ],
+        'país': 'México',
+        anios_experiecia: 12,
+        id_instructor: '4'
+    }, {
+        nombre: 'Vanesa',
+        apellido: 'Santos Martinez',
+        email: 'vanesa@hotmail.com',
+        especialidad: 'jazz',
+        certificaciones: [
+            'licenciatura en danza',
+            'técnicas jazz',
+            'danza musical',
+            'coreografía avanzada'
+        ],
+        'país': 'Chile',
+        anios_experiecia: 14,
+        id_instructor: '5'
+    }, {
+        nombre: 'Jose',
+        apellido: 'Vargas Mendez',
+        email: 'jose.v@outlook.com',
+        especialidad: 'salsa',
+        certificaciones: [
+            'licenciatura en danza',
+            'técnicas de salsa',
+            'coreografía avanzada',
+            'pedagogía y didáctica'
+        ],
+        'país': 'Perú',
+        anios_experiecia: 16,
+        id_instructor: '6'
+    }, {
+        nombre: 'Juliana',
+        apellido: 'Pérez Villamizar',
+        email: 'juliana.p@gmail.com',
+        especialidad: 'danza urbana',
+        certificaciones: [
+            'licenciatura en danza urbana',
+            'hip hop avanzado',
+            'pedagogía y didáctica',
+            'coreografía avanzada'
+        ],
+        'país': 'Colombia',
+        anios_experiecia: 10,
+        id_instructor: '7'
+    }, {
+        nombre: 'Oscar David',
+        apellido: 'Alarcon Rojas',
+        email: 'oscar@hotmail.com',
+        especialidad: 'breakdance',
+        certificaciones: [
+            'licenciatura en danza urbana',
+            'breakdance avanzado',
+            'anatomía y prevención de lesiones',
+            'coreografía'
+        ],
+        'país': 'Venezuela',
+        anios_experiecia: 13,
+        id_instructor: '8'
+    }, {
+        nombre: 'Maria',
+        apellido: 'Gutiérrez Sanchez',
+        email: 'maria.g@gmail.com',
+        especialidad: 'tango',
+        certificaciones: [
+            'licenciatura en danza',
+            'tango avanzado',
+            'coreografía',
+            'pedagogía y didáctica'
+        ],
+        'país': 'Argentina',
+        anios_experiecia: 11,
+        id_instructor: '9'
+    }, {
+        nombre: 'Ramiro',
+        apellido: 'Ruiz Flores',
+        email: 'ramiro@hotmail.com',
+        especialidad: 'samba',
+        certificaciones: [
+            'licenciatura en danza',
+            'samba avanzado',
+            'coreografía',
+            'pedagogía y didáctica'
+        ],
+        'país': 'Brasil',
+        anios_experiecia: 12,
+        id_instructor: '10'
+    }
+])
