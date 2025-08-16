9daf3d73b36f02afb4aba6fd2341a3440f13a287
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-08-15 23:17:30 -0500

    feat: :sparkles: insert eventos
    insert info about eventos
    
    By@CamilaFlorez

---
+++ eventos.js
@@ -0,0 +1,243 @@
+db.eventos.insertMany([
+    {
+        nombre_evento: 'Competencia folklorworld en parejas',
+        estilo_principal: 'folklore',
+        ciudad: 'Bucaramanga',
+        fecha: new Date("2025-10-21"),
+        participantes: [
+            {
+                nombre: 'Luna Camila',
+                apellido: 'Rojas Sanchez',
+                estilos: [
+                    'ballet',
+                    'salsa',
+                    'bambuco',
+                    'joropo'
+                ]
+            },
+            {
+                nombre: 'Daniel',
+                apellido: 'Vargas Morales',
+                estilos: [
+                    'salsa',
+                    'tango',
+                    'folklore'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Competencia Ballet Intermedia',
+        estilo_principal: 'ballet',
+        ciudad: 'Bogotá',
+        fecha: new Date("2025-09-15"),
+        participantes: [
+            {
+                nombre: 'Valentina',
+                apellido: 'Martínez Torres',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'jazz'
+                ]
+            },
+            {
+                nombre: 'Camila Sofía',
+                apellido: 'Pérez Jiménez',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'danza urbana'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Torneo Hip Hop Avanzado',
+        estilo_principal: 'hip hop',
+        ciudad: 'Medellín',
+        fecha: new Date("2025-09-20"),
+        participantes: [
+            {
+                nombre: 'Mateo Andrés',
+                apellido: 'Gómez Pérez',
+                estilos: [
+                    'hip hop',
+                    'breakdance',
+                    'locking'
+                ]
+            },
+            {
+                nombre: 'Lucas',
+                apellido: 'Ortiz Ramírez',
+                estilos: [
+                    'hip hop'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Festival Salsa y Bachata',
+        estilo_principal: 'salsa',
+        ciudad: 'Cali',
+        fecha: new Date("2025-09-25"),
+        participantes: [
+            {
+                nombre: 'Santiago',
+                apellido: 'Ramírez López',
+                estilos: [
+                    'salsa',
+                    'bachata',
+                    'merengue'
+                ]
+            },
+            {
+                nombre: 'Daniel',
+                apellido: 'Vargas Morales',
+                estilos: [
+                    'salsa',
+                    'tango',
+                    'folklore'
+                ]
+            },
+            {
+                nombre: 'Mariana',
+                apellido: 'Gutiérrez Flores',
+                estilos: [
+                    'salsa',
+                    'ballet',
+                    'danza contemporánea'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Competencia Contemporáneo Individual',
+        estilo_principal: 'contemporáneo',
+        ciudad: 'Buenos Aires',
+        fecha: new Date("2025-10-05"),
+        participantes: [
+            {
+                nombre: 'Valentina',
+                apellido: 'Martínez Torres',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'jazz'
+                ]
+            },
+            {
+                nombre: 'Isabella',
+                apellido: 'Hernández Cruz',
+                estilos: [
+                    'contemporáneo',
+                    'jazz',
+                    'hip hop'
+                ]
+            },
+            {
+                nombre: 'Camila Sofía',
+                apellido: 'Pérez Jiménez',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'danza urbana'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Competencia Breakdance Avanzado',
+        estilo_principal: 'breakdance',
+        ciudad: 'Caracas',
+        fecha: new Date("2025-10-12"),
+        participantes: [
+            {
+                nombre: 'Mateo Andrés',
+                apellido: 'Gómez Pérez',
+                estilos: [
+                    'hip hop',
+                    'breakdance',
+                    'locking'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Festival Tango y Folklore',
+        estilo_principal: 'tango',
+        ciudad: 'Quito',
+        fecha: new Date("2025-10-18"),
+        participantes: [
+            {
+                nombre: 'Julián',
+                apellido: 'Sánchez Díaz',
+                estilos: [
+                    'tango',
+                    'folklore',
+                    'samba'
+                ]
+            },
+            {
+                nombre: 'Daniel',
+                apellido: 'Vargas Morales',
+                estilos: [
+                    'salsa',
+                    'tango',
+                    'folklore'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Competencia Samba',
+        estilo_principal: 'samba',
+        ciudad: 'Río de Janeiro',
+        fecha: new Date("2025-10-22"),
+        participantes: [
+            {
+                nombre: 'Julián',
+                apellido: 'Sánchez Díaz',
+                estilos: [
+                    'tango',
+                    'folklore',
+                    'samba'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Festival Jazz Individual',
+        estilo_principal: 'jazz',
+        ciudad: 'Santiago',
+        fecha: new Date("2025-11-01"),
+        participantes: [
+            {
+                nombre: 'Valentina',
+                apellido: 'Martínez Torres',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'jazz'
+                ]
+            },
+            {
+                nombre: 'Isabella',
+                apellido: 'Hernández Cruz',
+                estilos: [
+                    'contemporáneo',
+                    'jazz',
+                    'hip hop'
+                ]
+            }
+        ]
+    }, {
+        nombre_evento: 'Competencia Danza Urbana',
+        estilo_principal: 'danza urbana',
+        ciudad: 'Medellín',
+        fecha: new Date("2025-11-10"),
+        participantes: [
+            {
+                nombre: 'Camila Sofía',
+                apellido: 'Pérez Jiménez',
+                estilos: [
+                    'ballet',
+                    'contemporáneo',
+                    'danza urbana'
+                ]
+            }
+        ]
+    }
+])
